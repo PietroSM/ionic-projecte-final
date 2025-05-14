6426c44cf70e7773722b897b3dcbf00220b3f027
PietroSM <pedrosalamora@gmail.com>
2025-05-14 19:01:43 +0200

    habemus_Deplegament

--- src/app/shared/interceptors/base-url.interceptor.ts
+++ src/app/shared/interceptors/base-url.interceptor.ts
@@ -3,10 +3,11 @@ import { isDevMode } from '@angular/core';
 
 export const baseUrlInterceptor: HttpInterceptorFn = (req, next) => {
   const serverUrl = isDevMode()
-    ? 'http://localhost:8080'
+    ? 'http://vps-281e1278.vps.ovh.net:8081'
     : '';
   const reqClone = req.clone({
     url: `${serverUrl}/${req.url}`,
   });
   return next(reqClone);
 };
+// http://localhost:8080
